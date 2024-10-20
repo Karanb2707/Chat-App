9e9e2a95dd7f480da80ccb60f53ff2ab795df2f1
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-10-21 00:57:09 +0530

    Online Users is working Now

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,8 +1,12 @@
-import { createContext, useEffect, useState } from "react";
-import { useAuthContext } from "./AuthContext";
+import { createContext, useContext, useEffect, useState } from "react";
+import { AuthContext, useAuthContext } from "./AuthContext";
 import io from "socket.io-client";
 
-export const SocketContext = createContext();
+const SocketContext = createContext();
+
+export const useSocketContext = () =>{
+    return useContext(AuthContext);
+}
 
 export const SocketContextProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
@@ -11,9 +15,17 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000");
+            const socket = io("http://localhost:5000",{
+                query:{
+                    userId: authUser._id
+                }
+            });
 
             setSocket(socket);
+
+            socket.on("getOnlineUsers", (users) => {
+                setOnlineUsers(users);
+            });
 
             return () => socket.close();
         } else {
