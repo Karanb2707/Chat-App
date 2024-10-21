f0ef6c66beab5e4aae64baa40ad204b291394898
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-10-22 00:42:42 +0530

    Message At Same Time Is Done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from '../models/conversation.model.js'
 import Message from '../models/message.model.js';
+import { getReceiverSocketId, io } from '../socket/socket.js';
 
 
 export const sendMessage = async (req, res) => {
@@ -28,7 +29,13 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
-        await Promise.all([conversation.save(), newMessage.save()])
+        await Promise.all([conversation.save(), newMessage.save()]);
+
+        //Socket Functionality here
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
 
         res.status(201).json(newMessage);
 
@@ -42,7 +49,7 @@ export const getMessages = async (req, res) => {
     try {
 
         console.log('req.user:', req.user);  // Add this line
-        
+
         const { id: userToChatId } = req.params;
         const senderId = req.user._id;
 
@@ -50,7 +57,7 @@ export const getMessages = async (req, res) => {
             participants: { $all: [senderId, userToChatId] },
         }).populate("messages");
 
-        if(!conversation) return res.status(200).json([]);
+        if (!conversation) return res.status(200).json([]);
 
         const messages = conversation.messages;
 
