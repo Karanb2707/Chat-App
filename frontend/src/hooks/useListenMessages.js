3d2eaf1753a8a5c4ec02d3927a218414d773a884
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2024-10-24 23:26:16 +0530

    Notification Sound Added

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
-import { useSocketContext } from "../context/SocketContext"
+import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
+import notficationSound from "../assets/sounds/notification_sound.mp3";
 
 const useListenMessages = () => {
     const { socket } = useSocketContext();
@@ -9,6 +10,8 @@ const useListenMessages = () => {
     useEffect(() => {
         socket.on("newMessage", (newMessage) => {
             newMessage.shouldShake = true;
+            const sound = new Audio(notficationSound);
+            sound.play();
             setMessages([...messages, newMessage])
         })
 
